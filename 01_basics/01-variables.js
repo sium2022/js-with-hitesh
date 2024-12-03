9ef97e14678361a8b8c1c994a18e7565e48bf76a
sium <siumhaque1089@gmail.com>
2024-12-03 22:07:55 +0600

    first basic lesson completed

--- 01_basics/01-variables.js
+++ 01_basics/01-variables.js
@@ -1,11 +1,15 @@
 const accountId = 144553;
 let accountEmail = "sium@google.com";
 var accountPassword = "12345";
-
 accountCity = "Dhaka";
+let division;
 
 // accountId = 2; //This value can not changed for constant
 
+accountEmail = 'dsafjn@yhgmol.com'
+accountPassword = "abcdef"
+accountCity = "Mymensingh"
+
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, division]);
