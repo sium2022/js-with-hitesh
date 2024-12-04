47ff35a62980174af22fbf69540440394feff847
sium <siumhaque1089@gmail.com>
2024-12-04 09:57:26 +0600

    conversion of data types

---
+++ 01_basics/03-conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+//conversion anything into number
+let  valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+//value types of inputed data
+
+//"33" => 33f
+//"33abc" => NaN
+//true => 1
+
+let isLoggedIn = 1
+
+let convertInBoolean = Boolean(isLoggedIn)
+console.log(typeof convertInBoolean);
+console.log(convertInBoolean);
+
+//1 => true
+// 0 => false
+//"" => false
+//"sium" => true
