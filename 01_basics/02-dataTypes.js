3c6b9b346e7d79f7a61eabc6ef1dda384e5384a8
sium <siumhaque1089@gmail.com>
2024-12-04 09:33:44 +0600

    data types

--- 01_basics/02-dataTypes.js
+++ 01_basics/02-dataTypes.js
@@ -2,3 +2,19 @@
 
 //alert(3 + 3); //we are using node js, not browser
 
+//code readability should be high
+
+
+/*Data types
+
+-bigint => for big big number use this data type
+
+-string => "alphabetic data stored in this type IN this apostrophy comma"
+
+-boolean => true/ false type data 
+
+-null  => standalone value 
+
+-undefined => where value didn't assigned yet
+
+*/
